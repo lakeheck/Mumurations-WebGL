fef529e6dade68d254b795b0d49f921dea80e8d7
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-04 08:57:23 -0500

    update naming

--- fluidsim.js
+++ fluidsim.js
@@ -1,5 +1,371 @@
 
 'use strict';
+import * as THREE from 'three';
+import { GPUComputationRenderer } from 'compute';
+
+
+//// FLOCKING SIM FIRST ///////////////
+/* TEXTURE WIDTH FOR SIMULATION */
+const WIDTH = 200;
+
+const BIRDS = WIDTH * WIDTH;
+class BirdGeometry extends THREE.BufferGeometry {
+
+    constructor() {
+
+        super();
+
+        const trianglesPerBird = 3;
+        const triangles = BIRDS * trianglesPerBird;
+        const points = triangles * 3;
+
+        const vertices = new THREE.BufferAttribute( new Float32Array( points * 3 ), 3 );
+        const birdColors = new THREE.BufferAttribute( new Float32Array( points * 3 ), 3 );
+        const references = new THREE.BufferAttribute( new Float32Array( points * 2 ), 2 );
+        const birdVertex = new THREE.BufferAttribute( new Float32Array( points ), 1 );
+
+        this.setAttribute( 'position', vertices );
+        this.setAttribute( 'birdColor', birdColors );
+        this.setAttribute( 'reference', references );
+        this.setAttribute( 'birdVertex', birdVertex );
+
+        // this.setAttribute( 'normal', new Float32Array( points * 3 ), 3 );
+
+
+        let v = 0;
+
+        function verts_push() {
+
+            for ( let i = 0; i < arguments.length; i ++ ) {
+
+                vertices.array[ v ++ ] = arguments[ i ];
+
+            }
+
+        }
+
+        const wingsSpan = 20;
+
+        for ( let f = 0; f < BIRDS; f ++ ) {
+
+            // Body
+
+            verts_push(
+                0, - 0, - 20,
+                0, 4, - 20,
+                0, 0, 30
+            );
+
+            // Wings
+
+            verts_push(
+                0, 0, - 15,
+                - wingsSpan, 0, 0,
+                0, 0, 15
+            );
+
+            verts_push(
+                0, 0, 15,
+                wingsSpan, 0, 0,
+                0, 0, - 15
+            );
+
+        }
+
+        for ( let v = 0; v < triangles * 3; v ++ ) {
+
+            const triangleIndex = ~ ~ ( v / 3 );
+            const birdIndex = ~ ~ ( triangleIndex / trianglesPerBird );
+            const x = ( birdIndex % WIDTH ) / WIDTH;
+            const y = ~ ~ ( birdIndex / WIDTH ) / WIDTH;
+
+            const c = new THREE.Color(
+                0x444444 +
+                ~ ~ ( v / 9 ) / BIRDS * 0x666666
+            );
+
+            birdColors.array[ v * 3 + 0 ] = c.r;
+            birdColors.array[ v * 3 + 1 ] = c.g;
+            birdColors.array[ v * 3 + 2 ] = c.b;
+
+            references.array[ v * 2 ] = x;
+            references.array[ v * 2 + 1 ] = y;
+            
+            birdVertex.array[ v ] = v % 9;
+            
+        }
+        
+        this.scale( 0.2, 0.2, 0.2 );
+        
+    }
+    
+}
+
+let container, stats;
+let camera, scene, renderer;
+let mouseX = 0, mouseY = 0;
+
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
+const BOUNDS = 800, BOUNDS_HALF = BOUNDS / 2;
+
+let last = performance.now();
+
+let gpuCompute;
+let velocityVariable;
+let positionVariable;
+let positionUniforms;
+let velocityUniforms;
+let birdUniforms;
+
+let renderTarget = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight );
+
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 150, 3000 );
+    camera.position.z = 350;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+    scene.fog = new THREE.Fog( 0xffffff, 100, 1000 );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    initComputeRenderer();
+
+
+    //display frame rate in corner 
+    // stats = new Stats();
+    // container.appendChild( stats.dom );
+
+    container.style.touchAction = 'none';
+    container.addEventListener( 'pointermove', onPointerMove );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    // const gui = new GUI();
+
+
+    // const effectController = {
+    //     separation: 20.0,
+    //     alignment: 20.0,
+    //     cohesion: 20.0,
+    //     freedom: 0.75
+    // };
+
+    // const valuesChanger = function () {
+
+    //     velocityUniforms[ 'separationDistance' ].value = effectController.separation;
+    //     velocityUniforms[ 'alignmentDistance' ].value = effectController.alignment;
+    //     velocityUniforms[ 'cohesionDistance' ].value = effectController.cohesion;
+    //     velocityUniforms[ 'freedomFactor' ].value = effectController.freedom;
+
+    // };
+
+    // valuesChanger();
+
+    // gui.add( effectController, 'separation', 0.0, 100.0, 1.0 ).onChange( valuesChanger );
+    // gui.add( effectController, 'alignment', 0.0, 100, 0.001 ).onChange( valuesChanger );
+    // gui.add( effectController, 'cohesion', 0.0, 100, 0.025 ).onChange( valuesChanger );
+    // gui.close();
+
+    initBirds();
+
+}
+function initComputeRenderer() {
+
+    gpuCompute = new GPUComputationRenderer( WIDTH, WIDTH, renderer );
+
+    if ( renderer.capabilities.isWebGL2 === false ) {
+
+        gpuCompute.setDataType( THREE.HalfFloatType );
+
+    }
+
+    const dtPosition = gpuCompute.createTexture();
+    const dtVelocity = gpuCompute.createTexture();
+    fillPositionTexture( dtPosition );
+    fillVelocityTexture( dtVelocity );
+
+    velocityVariable = gpuCompute.addVariable( 'textureVelocity', document.getElementById( 'fragmentShaderVelocity' ).textContent, dtVelocity );
+    positionVariable = gpuCompute.addVariable( 'texturePosition', document.getElementById( 'fragmentShaderPosition' ).textContent, dtPosition );
+
+    gpuCompute.setVariableDependencies( velocityVariable, [ positionVariable, velocityVariable ] );
+    gpuCompute.setVariableDependencies( positionVariable, [ positionVariable, velocityVariable ] );
+
+    positionUniforms = positionVariable.material.uniforms;
+    velocityUniforms = velocityVariable.material.uniforms;
+
+    positionUniforms[ 'time' ] = { value: 0.0 };
+    positionUniforms[ 'delta' ] = { value: 0.0 };
+    velocityUniforms[ 'time' ] = { value: 1.0 };
+    velocityUniforms[ 'delta' ] = { value: 0.0 };
+    velocityUniforms[ 'testing' ] = { value: 1.0 };
+    velocityUniforms[ 'separationDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'alignmentDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'cohesionDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'freedomFactor' ] = { value: 1.0 };
+    velocityUniforms[ 'predator' ] = { value: new THREE.Vector3() };
+    velocityVariable.material.defines.BOUNDS = BOUNDS.toFixed( 2 );
+
+    velocityVariable.wrapS = THREE.RepeatWrapping;
+    velocityVariable.wrapT = THREE.RepeatWrapping;
+    positionVariable.wrapS = THREE.RepeatWrapping;
+    positionVariable.wrapT = THREE.RepeatWrapping;
+
+    const error = gpuCompute.init();
+
+    if ( error !== null ) {
+
+        console.error( error );
+
+    }
+
+}
+function initBirds() {
+
+    const geometry = new BirdGeometry();
+
+    // For Vertex and Fragment
+    birdUniforms = {
+        'color': { value: new THREE.Color( 0xff2200 ) },
+        'texturePosition': { value: null },
+        'textureVelocity': { value: null },
+        'time': { value: 1.0 },
+        'delta': { value: 0.0 }
+    };
+
+    // THREE.ShaderMaterial
+    const material = new THREE.ShaderMaterial( {
+        uniforms: birdUniforms,
+        vertexShader: document.getElementById( 'birdVS' ).textContent,
+        fragmentShader: document.getElementById( 'birdFS' ).textContent,
+        side: THREE.DoubleSide
+
+    } );
+
+    const birdMesh = new THREE.Mesh( geometry, material );
+    birdMesh.rotation.y = Math.PI / 2;
+    birdMesh.matrixAutoUpdate = false;
+    birdMesh.updateMatrix();
+
+    scene.add( birdMesh );
+
+}
+function fillPositionTexture( texture ) {
+
+    const theArray = texture.image.data;
+
+    for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
+
+        const x = Math.random() * BOUNDS - BOUNDS_HALF;
+        const y = Math.random() * BOUNDS - BOUNDS_HALF;
+        const z = Math.random() * BOUNDS - BOUNDS_HALF;
+
+        theArray[ k + 0 ] = x;
+        theArray[ k + 1 ] = y;
+        theArray[ k + 2 ] = z;
+        theArray[ k + 3 ] = 1;
+
+    }
+
+}
+function fillVelocityTexture( texture ) {
+
+    const theArray = texture.image.data;
+
+    for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
+
+        const x = Math.random() - 0.5;
+        const y = Math.random() - 0.5;
+        const z = Math.random() - 0.5;
+
+        theArray[ k + 0 ] = x * 10;
+        theArray[ k + 1 ] = y * 10;
+        theArray[ k + 2 ] = z * 10;
+        theArray[ k + 3 ] = 1;
+
+    }
+
+}
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+function onPointerMove( event ) {
+
+    if ( event.isPrimary === false ) return;
+
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+    
+}
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    flockRender();
+    // stats.update();
+
+}
+
+//render flock to a texture buffer
+function flockRender() {
+    
+    const now = performance.now();
+    let delta = ( now - last ) / 1000;
+    
+    if ( delta > 1 ) delta = 1; // safety cap on large deltas
+    last = now;
+    
+    positionUniforms[ 'time' ].value = now;
+    positionUniforms[ 'delta' ].value = delta;
+    velocityUniforms[ 'time' ].value = now;
+    velocityUniforms[ 'delta' ].value = delta;
+    birdUniforms[ 'time' ].value = now;
+    birdUniforms[ 'delta' ].value = delta;
+    
+    velocityUniforms[ 'predator' ].value.set( 0.5 * mouseX / windowHalfX, - 0.5 * mouseY / windowHalfY, 0 );
+    
+    mouseX = 10000;
+    mouseY = 10000;
+    
+    gpuCompute.compute();
+
+    birdUniforms[ 'texturePosition' ].value = gpuCompute.getCurrentRenderTarget( positionVariable ).texture;
+    birdUniforms[ 'textureVelocity' ].value = gpuCompute.getCurrentRenderTarget( velocityVariable ).texture;
+    
+
+    //TODO - package up the scene as a module that can be imported and return a texture of the scene with some easy to configure paraemters 
+    //below, for rendering to a scene 
+    renderer.setRenderTarget( renderTarget );
+    renderer.render( scene, camera );
+    // renderer.setRenderTarget( null );
+
+    // renderer.render( scene, camera );
+
+}
+
 
 // Mobile promo section
 
@@ -244,7 +610,7 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 // }
 
 function startGUI () {
-    const parName = 'Output Resolution';
+    const parName = 'Output Resolution 1';
     //dat is a library developed by Googles Data Team for building JS interfaces. Needs to be included in project directory 
     var gui = new dat.GUI({ width: 300 });
 
@@ -1439,6 +1805,9 @@ let noise;
 //load texture for dithering
 let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
 let picture = createTextureAsync('img/flowers_fence.JPG');
+
+// let murmur = createFBORenderTarget(renderTarget);
+
 // let picture = createTextureAsync('img/lake-heckaman-IMG_0997-dec-2022.jpg');
 // console.log('loaded picture successfully');
 
@@ -1578,6 +1947,7 @@ function createFBO (w, h, internalFormat, format, type, param) {
         }
     };
 }
+
 
 function createDoubleFBO (w, h, internalFormat, format, type, param) {
     let fbo1 = createFBO(w, h, internalFormat, format, type, param);
@@ -1803,7 +2173,10 @@ function step (dt) {
         splatVelProgram.bind();
         gl.uniform1i(splatVelProgram.uniforms.uTarget, velocity.read.attach(0)); 
         // gl.uniform1i(splatVelProgram.uniforms.uTarget, velocity.read.attach(0));
-        gl.uniform1i(splatVelProgram.uniforms.uDensityMap, picture.attach(1)); //density map
+
+        gl.activeTexture(gl.TEXTURE0 + 1);
+        gl.bindTexture(gl.TEXTURE_2D, picture.texture);
+        gl.uniform1i(splatVelProgram.uniforms.uDensityMap, picture.texture); //density map
         gl.uniform1i(splatVelProgram.uniforms.uForceMap, noise.read.attach(2)); //add noise for velocity map 
         gl.uniform1f(splatVelProgram.uniforms.aspectRatio, canvas.width / canvas.height);
         gl.uniform1f(splatVelProgram.uniforms.uVelocityScale, config.VELOCITYSCALE);
